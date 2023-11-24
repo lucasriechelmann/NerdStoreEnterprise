7acb49d41d1c16b790165fc2990b1e8da4ee1829
Lucas Riechelmann Ramos <lucas.riechelmann@hotmail.com>
2023-11-24 08:09:54 +0000

    adjusted basket

---
+++ src/web/NSE.WebApp.MVC/Extensions/BasketViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class BasketViewComponent : ViewComponent
+    {
+        private readonly IBasketService _basketService;
+        public BasketViewComponent(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _basketService.GetBasket() ?? new BasketViewModel());
+        }
+    }
+}
